35ff9926eb1ea94da761a07c73d9917c4d313176
louhugh <1305525581@qq.com>
2016-07-20 11:15:17 +0800

    haha2

--- app/src/main/java/mydemo/com/loughu/cn/gitdemo007/MainActivity.java
+++ app/src/main/java/mydemo/com/loughu/cn/gitdemo007/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("上传成功哈哈");
+        System.out.println("上传成功哈哈");
     }
 }
