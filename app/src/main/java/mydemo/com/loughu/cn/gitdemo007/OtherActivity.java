66fe166e8c8b3f2d96cb8a3f53681c7e22ff6125
louhugh <1305525581@qq.com>
2016-07-20 10:30:26 +0800

    002

---
+++ app/src/main/java/mydemo/com/loughu/cn/gitdemo007/OtherActivity.java
@@ -0,0 +1,15 @@
+package mydemo.com.loughu.cn.gitdemo007;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class OtherActivity  extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
